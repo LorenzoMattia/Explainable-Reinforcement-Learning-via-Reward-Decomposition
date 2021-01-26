5d002dfa412b06edee7bb962206ba42d3b7c68a5
norton2000 <63303760+norton2000@users.noreply.github.com>
2021-01-26 13:04:13 +0100

    Prima versione Funzionante di Q-Learning con NN

--- agente.py
+++ agente.py
@@ -4,14 +4,19 @@ import lunar_lander
 from lunar_lander import LunarLander
 
 env = LunarLander()
-nactions = env.action_space.n
+num_actions = env.action_space.n
+num_features = env.observation_space.shape[0]
+print('Number of state features: {}'.format(num_features))
+print('Number of possible actions: {}'.format(num_actions))
+
+
 gamma = 0.99
 
 class MyModel(tf.keras.Model):
     def __init__(self, state_shape, hidden_units, num_actions):
         super(MyModel, self).__init__()
         self.input_layer = tf.keras.layers.InputLayer(
-            input_shape=(8,)
+            input_shape=state_shape
         )
         self.hidden_layer = tf.keras.layers.Dense(
             hidden_units, activation='relu', kernel_initializer='RandomNormal')
@@ -26,26 +31,46 @@ class MyModel(tf.keras.Model):
         return output
 
 # Output is three values, Q for all possible actions
-Qs = MyModel(8, 64, nactions)
+Qs = MyModel(num_features, 64, num_actions)
 
 # Optimizer adjusts weights to minimize loss, with the speed of learning_rate
 optimizer = tf.optimizers.Adam(1e-3)
 
 epsilon = 0.1
+discount = 0.99
 max_episodes = 200
 max_episode_length = 2000
 eye = np.eye(8)
 
+def demo_lander(env, seed=None, render=False):
+    env.seed(seed)
+    total_reward = np.zeros(8)
+    steps = 0
+    s = env.reset()
+    while True:
+        a = policy(1, s)
+        s, r, done, info = env.step(a[0])
+        total_reward += r
+
+        if render:
+            still_open = env.render()
+            if still_open == False: break
+
+        if steps % 20 == 0 or done:
+            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
+            print("step {} total_reward {}".format(steps, total_reward))
+        steps += 1
+        if done: break
+    return total_reward
+
 def predict(state):
-    print("-------------------")
-    print(np.shape(state))
-    print("-------------------")
-    return Qs(state)
+    state_in = tf.expand_dims(state, axis=0)
+    return Qs(state_in)
 
 def policy(eps, state):
     vals = predict(state)
     #nostra policy
-    a = np.argmax(vals, axis=-1) if np.random.rand() >= eps  else np.random.randint(nactions, size=vals.shape[0])
+    a = np.argmax(vals, axis=-1) if np.random.rand() >= eps  else np.random.randint(num_actions, size=vals.shape[0])
     return a
 
 for ep in range(max_episodes):
@@ -56,19 +81,23 @@ for ep in range(max_episodes):
     d = False
 
     while not d:      
-        a = policy(epsilon, current_state)
+        a = policy(epsilon, current_state)    
         o, r, d, _ = env.step(a[0])
-        next_state = preprocess_state(o)
+        next_state = o
 
-        rew = np.sum(s)
+        rew = np.sum(r)
         
         with tf.GradientTape() as tape:
+          
           selected_action_values = tf.math.reduce_sum(        # Q(s, a)
-              predict(current_state) * tf.one_hot(a, nactions), axis=1)
-          target = tf.stop_gradient(rew + (1 - d)*tf.reduce_max(predict(next_state), axis=1))  # r + (1 - d)*max(Q(s', a))
+              predict(current_state) * tf.one_hot(a, num_actions), axis=1)
+          
+          target = tf.stop_gradient(rew + (1 - d)*discount*tf.reduce_max(predict(next_state), axis=1))  # r + (1 - d)*γ*max(Q(s', a))
           loss = tf.math.reduce_mean(tf.square(target - selected_action_values))
         variables = Qs.trainable_variables
         gradients = tape.gradient(loss, variables)
         optimizer.apply_gradients(zip(gradients, variables))
 
         current_state = next_state
+
+demo_lander(env, render=True)
